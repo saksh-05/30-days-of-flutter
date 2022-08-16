e0737c49fd539bc5663aaec0dc7a0987369c8a76
Saurabh Kumar singh <pcsingh050301@gmail.com>
2022-08-17 02:29:27 +0530

    day09

--- dev_flutter/lib/screen/day_06.dart
+++ dev_flutter/lib/screen/day_06.dart
@@ -1,114 +1,3 @@
-// stateless widget
-// import 'dart:math';
-
-// import 'package:flutter/material.dart';
-
-// class Day05 extends StatelessWidget {
-//   // const ({ Key? key }) : super(key: key);
-//   // const string side = "Left";
-//   Random rnd = Random();
-//   List<String> col = [
-//     "#053ca4",
-//     "#059112",
-//     "#05afb0",
-//     "#a505b0",
-//     "#e92f75",
-//     "#f11d26",
-//     "#f7a03d"
-//   ];
-//   var clr = Color(int.parse("0xfffff"));
-//   void changeBackground() {
-//     String val = col[rnd.nextInt(7)].replaceAll("#", "0xff");
-//     clr = Color(int.parse(val));
-//     print(col[rnd.nextInt(7)]);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Day05"),
-//         backgroundColor: const Color.fromARGB(255, 3, 80, 118),
-//       ),
-//       drawer: Drawer(
-//         child: Column(
-//           children: [
-//             const DrawerHeader(
-//               decoration: BoxDecoration(
-//                 color: Color.fromARGB(255, 3, 80, 118),
-//               ),
-//               margin: EdgeInsets.zero,
-//               padding: EdgeInsets.zero,
-//               child: Center(
-//                 child: Text(
-//                   "Day 05 Drawer",
-//                   textScaleFactor: 1.5,
-//                   style: TextStyle(
-//                     color: Colors.white,
-//                     fontWeight: FontWeight.w800,
-//                   ),
-//                 ),
-//               ),
-//             ),
-//             Expanded(
-//               child: MediaQuery.removePadding(
-//                 context: context,
-//                 removeTop: true,
-//                 child: ListView.separated(
-//                   itemCount: 31,
-//                   itemBuilder: (context, index) {
-//                     return index != 0
-//                         ? ListTile(
-//                             title: Text(
-//                                 index + 1 < 10 ? 'Day-0$index' : 'Day-$index'),
-//                             tileColor: Colors.amberAccent[700],
-//                             textColor: Colors.white,
-//                             onTap: () {
-//                               Navigator.pushNamed(
-//                                   context,
-//                                   index + 1 < 10
-//                                       ? '/day0$index'
-//                                       : '/day$index');
-//                             },
-//                           )
-//                         : ListTile(
-//                             title: const Text("Home"),
-//                             subtitle: const Text('Go to home screen'),
-//                             tileColor: Colors.amberAccent[700],
-//                             textColor: Colors.white,
-//                             onTap: () {
-//                               Navigator.pushNamed(context, "/");
-//                             },
-//                           );
-//                   },
-//                   separatorBuilder: (context, index) => const Divider(
-//                     thickness: 2,
-//                   ),
-//                 ),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//       body: Container(
-//         decoration: BoxDecoration(color: clr),
-//         child: Center(
-//           child: ElevatedButton(
-//             style: ButtonStyle(
-//               backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-//               foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
-//             ),
-//             child: const Text("Press to change background"),
-//             onPressed: changeBackground,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
 class Day06 extends StatefulWidget {
@@ -122,22 +11,11 @@ class Day06 extends StatefulWidget {
 class Day06State extends State<Day06> {
   // const ({ Key? key }) : super(key: key);
   // const string side = "Left";
-  Random rnd = Random();
-  List<String> col = [
-    "#053ca4",
-    "#059112",
-    "#05afb0",
-    "#a505b0",
-    "#e92f75",
-    "#f11d26",
-    "#f7a03d"
-  ];
-  var clr = Color(int.parse("0xfffff"));
-  void changeBackground() {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(index) {
     setState(() {
-      String val = col[rnd.nextInt(7)].replaceAll("#", "0xff");
-      clr = Color(int.parse(val));
-      print(col[rnd.nextInt(7)]);
+      _selectedIndex = index;
     });
   }
 
@@ -209,6 +87,36 @@ class Day06State extends State<Day06> {
             ],
           ),
         ),
-        body: Text("Flutter layouts: Listview, Gridview, Slivers"));
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Page View',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'ListView',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Grid View',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Sliver List',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Sliver Grid',
+            ),
+          ],
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber,
+          unselectedItemColor: Colors.white,
+          backgroundColor: const Color.fromARGB(255, 3, 80, 118),
+          onTap: _onItemTapped,
+        ),
+        body: const Text("Flutter layouts: Listview, Gridview, Slivers"));
   }
 }
